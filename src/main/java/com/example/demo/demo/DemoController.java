ca3395292732be6ad9956d19c7e61d16fbb51d4f
Karan <karankukreti20@gmail.com>
2024-03-21 08:14:21 +0530

    Removing unnecessary comments

--- src/main/java/com/example/demo/demo/DemoController.java
+++ src/main/java/com/example/demo/demo/DemoController.java
@@ -11,13 +11,11 @@ public class DemoController {
         return ResponseEntity.ok("Hello, Admin!");
     }
 
-    // Generic greeting endpoint for all users
     @GetMapping("/greetVisitor")
     public ResponseEntity<String> greet() {
         return ResponseEntity.ok("Hello, Visitor!");
     }
 
-    // Endpoint for Admin to greet a specific user
     @GetMapping("/greet/adminOrUser")
     public ResponseEntity<String> greetUser() {
         return ResponseEntity.ok("Hello, User Or Admin!");
